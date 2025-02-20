7d745a009cbd18c0e6ec49f6f42c6e47223cbc4d
Chaun09 <bangcac24@gmail.com>
2025-02-20 14:10:55 +0700

    first commit

---
+++ be-crud-3obj/src/main/java/f14/example/be_crud_3obj/BeCrud3objApplication.java
@@ -0,0 +1,13 @@
+package f14.example.be_crud_3obj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeCrud3objApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeCrud3objApplication.class, args);
+	}
+
+}
---
+++ be-crud-3obj/src/test/java/f14/example/be_crud_3obj/BeCrud3objApplicationTests.java
@@ -0,0 +1,13 @@
+package f14.example.be_crud_3obj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeCrud3objApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
